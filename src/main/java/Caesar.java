3ecd24cfe5a9ccf73e1a3ef9f5514f1a06116901
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-12-27 21:19:16 +0300

    Add a constructor method to define the blueprint class of the cipher and make the fields private

---
+++ src/main/java/Caesar.java
@@ -0,0 +1,9 @@
+class Caesar {
+    private String plainText;
+    private int shift;
+
+    public Caesar(String plainText, int shift){
+        this.plainText = plainText;
+        this.shift = shift;
+    }
+}
