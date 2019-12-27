f0b509f6839987c2fabb0c6912c4e767cc3810c7
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-12-27 21:27:32 +0300

    Create a jUnit test file

---
+++ src/test/java/CaesarTest.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CaesarTest {
+
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
