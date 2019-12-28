eaf71653d7d77ae3fdafe4750a0dd0a8049c62f0
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-12-28 10:00:49 +0300

    Create a test file and test the getPlainText getter method

--- src/test/java/CaesarTest.java
+++ src/test/java/CaesarTest.java
@@ -3,6 +3,26 @@ import static org.junit.Assert.*;
 
 public class CaesarTest {
 
+    @Test
+    public void runCipher_returnString_String(){
+        Caesar testCipher = new Caesar("nebuchadnezzer",2);
+        String expectedOutput = new String();
+        assertEquals(expectedOutput, testCipher);
+
+    }
+
+
+//    @Test
+//    public void runPingPong_replaceMultiplesOf5_ArrayList() {
+//        PingPong testPingPong = new PingPong();
+//        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+//        expectedOutput.add(1);
+//        expectedOutput.add(2);
+//        expectedOutput.add("ping");
+//        expectedOutput.add(4);
+//        expectedOutput.add("pong");
+//        assertEquals(expectedOutput, testPingPong.runPingPong(5));
+//    }
 
 
     @Before
