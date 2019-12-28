0eab0280bb1e8243775f5bb592445abea6371420
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-12-28 20:00:27 +0300

    Add code for output text asking for the plainText and store it in a string object inputText

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,14 @@ import java.util.*;
 public class App {
     public static void main(String[] args){
         Console myConsole = System.console();
-    }
 
+            System.out.println("This is Caesar's Cipher, enter a text and we shall encrypt it for you: ");
+
+            String inputText = myConsole.readLine();
+
+
+
+
+    }
 }
 
